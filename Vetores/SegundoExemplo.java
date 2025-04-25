ced9bba16119d4253b057e04413d86ba7188eb6f
Layza Nicolle <129307127+LayzaNicolle@users.noreply.github.com>
2025-04-25 19:40:54 +0000

    Usando for each e colocando o vetor em ordem

--- Vetores/SegundoExemplo.java
+++ Vetores/SegundoExemplo.java
@@ -1,5 +1,18 @@
+//usando o for-each
+import java.util.Arrays;
 public class SegundoExemplo {
+    public static void main(String[] args){
+        //criando o vetor
+        double v[] = {3.5, 2.75, 4.5 };
 
+        //para colocar o vetor em ordem
+        Arrays.sort(v);
     
+        for(double valor: v){
+            System.out.println(valor);
+        }
+
+
+    }
     
 }
